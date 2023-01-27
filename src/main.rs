d96ada76d5f3afa53d449690845fa8563146b018
Eugene Masaka <eugenemasaka777@gmail.com>
2023-01-28 00:52:00 +0300

    styling ui

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use stylist::yew::use_style;
+use stylist::{yew::styled_component, style};
 use yew::prelude::*;
 struct Blog {
 	token: String,
@@ -12,22 +12,46 @@ fn app()-> Html {
 	let videos: Vec<String> = vec![];
 	let card_style = style!(
 		r#"
-		height: 55vh;
-		width: 40vw;
-		background_color: black;
+		box-shadow: 0 4px 8px 0 rgba(0,0,0,0.3);
+		height: 200px;
+		width: 30%;
+		margin-top: 75px;
+		border-radius: 18px;
+		background-color: black;
 		color: white;
-		border_radius: 24px;
+		"#
+		).expect("Failed to mount style");
+	let main_content_style = style!(
+			r#"
+			height: 100%;
+			width: 100%;
+			display: flex;
+			align-content: center;
+			justify-content: center;
+			text-align: center;
+			"#
+		).expect("Failed to mount style");
+	let button_style = style!(
+		r#"
+		text-decoration: none;
+		padding: 5px 10px 5px 10px;
+		color: white;
+		background-color: blue;
+		border-radius: 13px;
+		margin-top: 45px;
 		"#
 		).expect("Failed to mount style");
 	html! {
 		<>
-		<div class={card_style} style="width: 18rem;">
+		<div class = {main_content_style}>
+		<div class={card_style}>
  	    <img />
   		<div class="card-body">
     	<h1 class="card-title">{"Welcome to blog3.0"}</h1>
     	<p class="card-text">{"Connect Wallet To Access the Site"}</p>
-    	<a href="#" class="btn btn-primary">{"Connect Wallet"}</a>
+    	<a href="#" class={button_style}>{"Connect Wallet"}</a>
   		</div>
+		</div>
 		</div>
 		</>
 	}
